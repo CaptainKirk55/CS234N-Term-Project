c4bd36272ee2559b9f2e8e884007d71d6d736fe9
Kirk <wyatt@kirk55.com>
2022-11-20 19:58:06 -0800

    Classes from Mari. Not working all the way

---
+++ CS243N Term Project/Models/KeyValueItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BITSClasses.AdditionalModels
+{
+    public class KeyValueItem
+    {
+        public int Key { get; set; }
+        public string Value { get; set; }
+
+        public KeyValueItem(int key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
