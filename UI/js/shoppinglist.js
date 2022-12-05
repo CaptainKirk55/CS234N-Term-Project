84724f851d970c198eccf9b719ea66d1a1c5a222
Kirk <wyatt@kirk55.com>
2022-12-04 21:07:43 -0800

    Add to cart and comments

--- UI/js/shoppinglist.js
+++ UI/js/shoppinglist.js
@@ -71,6 +71,7 @@ class ShoppingList {
         })
     } 
 
+    //Adds an item to the cart. In the future this will add to the cart of the proper supplier.
     AddToCart(buttonId) {
         let index = buttonId.substr(2); //Button ids are saved as IB(Ingredient ID). Substringing it gives us the ingredient id
         fetch(`${this.server}/ingredients/${index}`)
@@ -106,13 +107,12 @@ class ShoppingList {
     }
 
     //Render a new row on the cart
+    //In the future this will also show the weight and the cost of the item
     RenderNewCartElement(ingredient) {
         return `
             <tr>
                 <th scope="row">${ingredient.IngredientName}</th>
                 <td id="CI${ingredient.IngredientId}">${ingredient.Ordering}</td>
-                <td>n/a</td>
-                <td>n/a</td>
                 <td>${ingredient.OnHandQuantity}</td>
             </tr>
         `
@@ -120,9 +120,24 @@ class ShoppingList {
 
     //Re-render an old row of the cart and update the quantity
     RenderOldCartElement(index) {
-        document.getElementById("CI" + index).innerHTML = this.carts.ingredients[index - 1].Ordering;
+        console.log(index);
+        document.getElementById("CI" + this.carts.ingredients[index-1].IngredientId).innerHTML = this.carts.ingredients[index - 1].Ordering;
     }
 
+    //This function would render the cart head per supplier.
+    //I'd also have to rework how the cart element creation exists a little bit to account for multiple tables.
+    RenderCartHead(supplier) {
+
+    }
+
+    //This will render the total weight and cost of the current cart. It will be called every time something new is added to it.
+    //Will need cost and weight data for each item to do this.
+    //This may also indicate how close to a pallet completion the user is. That was one of the requested features.
+    RenderCartInfo(supplier) {
+
+    }
+
+    //This just activates the sort buttons.
     ActivateSortButtons() {
         this.adjunctButton.onclick = () => {
             this.populateIngredientBank(1);
@@ -181,6 +196,7 @@ class ShoppingList {
     }
     
     //Render the table headers of the ingredient bank
+    //In the future this will show which supplier, the weight, and the cost of the item.
     RenderIngredientBankTop() {
         return `
             <thead class="thead">
@@ -190,7 +206,22 @@ class ShoppingList {
                 </tr>
             </thead>
         `
-    }  
+    }
+
+    //This would post the order to the database and add everything to the incoming list. 
+    //It will also probably take the user to a new screen so they can input the incoming delivery date.
+    PostOrder() {
+
+    }
+
+    //This would show the alerts at the top of the page to indicate when items are beyond their reorder point, or will be when taking into account upcoming orders.
+    //It will prompt the user to add the items directly to their cart so they don't have to search the ingredient bank for it.
+    RenderAlerts() {
+
+    }
+
+    //More ways to sort the ingredient list. By quantity and by name. Also just a way to search directly for the name. 
+    //The ingredient list should also probably have a scroll bar of it's own instead of the main page scroll bar.
 }
 
 window.addEventListener("load", () => new ShoppingList);
